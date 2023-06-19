dd88c3a33d257e18bc0f8b9e28d752f1325c33af
fauzan-radji <triputrafauzan@gmail.com>
2023-06-19 23:26:01 +0800

    feat: dynamic todo using fetch

--- src/views/Todos.jsx
+++ src/views/Todos.jsx
@@ -1,44 +1,32 @@
+import { useEffect, useState } from "react";
 import Todo from "../components/Todo";
+import { useParams } from "react-router-dom";
+
+const endpoint = `http://${window.location.hostname}:${process.env.REACT_APP_API_PORT}`;
 
 export default function Todos() {
+  const [todos, setTodos] = useState([]);
+  const { listId } = useParams();
+
+  useEffect(() => {
+    fetch(endpoint + "/lists/" + listId + "/todos")
+      .then((res) => res.json())
+      .then((data) => {
+        setTodos(data);
+      });
+  }, []);
+
   return (
     <div className="flex flex-col gap-4">
-      <Todo
-        title="Very High Priority"
-        description="Very High Priority Descriptions"
-        importance="5"
-        deadline="28/02/2023"
-      />
-      <Todo
-        title="High Priority"
-        description="High Priority Descriptions"
-        importance="4"
-        deadline="28/02/2023"
-      />
-      <Todo
-        title="Medium Priority"
-        description="Medium Priority Descriptions"
-        importance="3"
-        deadline="28/02/2023"
-      />
-      <Todo
-        title="Low Priority"
-        description="Low Priority Descriptions"
-        importance="2"
-        deadline="03/03/2023"
-      />
-      <Todo
-        title="Very Low Priority"
-        description="Very Low Priority Descriptions"
-        importance="1"
-        deadline="28/02/2023"
-      />
-      <Todo
-        title="None Priority"
-        description="None Priority Descriptions"
-        importance="0"
-        deadline="28/02/2023"
-      />
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          description={todo.description}
+          importance={todo.importance}
+          deadline={todo.deadline}
+        />
+      ))}
     </div>
   );
 }
